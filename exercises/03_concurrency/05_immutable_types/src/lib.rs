cc787957f82a3a6527c48a9ae6fb35635e5fd2fd
LukeMathWalker <20745048+LukeMathWalker@users.noreply.github.com>
2025-01-15 14:33:54 +0100

    Immutable exercise

--- exercises/03_concurrency/05_immutable_types/src/lib.rs
+++ exercises/03_concurrency/05_immutable_types/src/lib.rs
@@ -1,24 +1,38 @@
 use pyo3::prelude::*;
 
-#[pyclass(frozen)]
-struct Point {
-    x: i32,
-    y: i32,
+#[pyclass]
+struct Rectangle {
+    width: u32,
+    length: u32,
+}
+
+#[pymethods]
+impl Rectangle {
+    #[new]
+    fn new(width: u32, length: u32) -> Self {
+        Self { width, length }
+    }
 }
 
 #[pyfunction]
-fn print_point<'py>(python: Python<'py>, point: Bound<'py, Point>) {
-    let point: Py<Point> = point.unbind();
+/// Compute the area of a rectangle while allowing Python to run other threads.
+/// Fill in the body of the function.
+/// Modify `Rectangle`'s definition if necessary.
+///
+/// # Constraints
+///
+/// Do NOT remove the `allow_threads` call. The computation must be done inside
+/// the closure passed to `allow_threads`.
+fn compute_area<'py>(python: Python<'py>, shape: Bound<'py, Rectangle>) -> u32 {
     python.allow_threads(|| {
-        // We can now access the fields of the Point struct
-        // even though we are not holding the GIL
-        let point: &Point = point.get();
-        println!("({}, {})", point.x, point.y);
-    });
+        let area: u32 = todo!();
+        area
+    })
 }
 
 #[pymodule]
 fn immutable(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(print_point, m)?)?;
+    m.add_function(wrap_pyfunction!(compute_area, m)?)?;
+    m.add_class::<Rectangle>()?;
     Ok(())
 }
