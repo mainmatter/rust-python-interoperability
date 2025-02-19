6e2eb73603dcc1ace0135f7f2b31fce7b71f0113
LukeMathWalker <20745048+LukeMathWalker@users.noreply.github.com>
2025-02-19 08:55:26 +0000

    Don't try to compile the Rust project at the very start. First create the venv

--- verifier/src/main.rs
+++ verifier/src/main.rs
@@ -3,10 +3,16 @@ use duct::cmd;
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Create the project-specific Python virtual environment.
     println!("Creating the Python virtual environment...");
-    cmd!("uv", "sync", "--all-packages").run()?;
+    cmd!("uv", "sync", "--no-install-workspace").run()?;
+
+    let venv = std::env::current_dir()?.join(".venv");
+    cmd!("uv", "sync", "--all-packages")
+        // Tell `pyo3` where to find the project-specific
+        // virtual environment.
+        .env("VIRTUAL_ENV", &venv)
+        .run()?;
 
     println!("Testing the Rust crate...");
-    let venv = std::env::current_dir()?.join(".venv");
     cmd!("cargo", "test")
         // Tell `pyo3` where to find the project-specific
         // virtual environment.
