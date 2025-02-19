925c4fade60b3f5f7d3cee32d04bcb77f7a35c56
LukeMathWalker <20745048+LukeMathWalker@users.noreply.github.com>
2025-02-19 12:16:57 +0000

    Use a faster path for verification if the .venv is already in place

--- verifier/src/main.rs
+++ verifier/src/main.rs
@@ -1,11 +1,14 @@
 use duct::cmd;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // Create the project-specific Python virtual environment.
-    println!("Creating the Python virtual environment...");
-    cmd!("uv", "sync", "--no-install-workspace").run()?;
-
     let venv = std::env::current_dir()?.join(".venv");
+
+    if venv.try_exists().ok() != Some(true) {
+        // Create the project-specific Python virtual environment.
+        println!("Creating the Python virtual environment...");
+        cmd!("uv", "sync", "--no-install-workspace").run()?;
+    }
+
     cmd!("uv", "sync", "--all-packages")
         // Tell `pyo3` where to find the project-specific
         // virtual environment.
